1dc5b6754ed2317073bae9b02ffa672d6ffc2ade
Merlyy1 <122520926+Merlyy1@users.noreply.github.com>
2024-09-30 10:03:25 +0200

    Update exo1.js

--- ajout_et_suppression_dom/exo1.js
+++ ajout_et_suppression_dom/exo1.js
@@ -5,6 +5,13 @@ button.addEventListener('click', function () {
     let newElement = document.createElement("div1");
     let para2 = document.getElementById("p2");
     para2.appendChild(newElement);
+    newElement.textContent = "<br>bonjour le monde";
+
+    /*let button = document.getElementById("monBouton");
+    button.addEventListener('click', function () {
+    let para = document.createElement("p");
+    let para2 = document.getElementById("p2");
+    para2.appendChild(newElement);
     newElement.innerHTML = "<br>bonjour le monde";
-    
+
 });
