1fc7b2e1c43b8bc0b5775c9322df85e87af049ce
Merlyy1 <122520926+Merlyy1@users.noreply.github.com>
2024-09-30 11:24:24 +0200

    Update exo4.js

--- ajout_et_suppression_dom/exo4.js
+++ ajout_et_suppression_dom/exo4.js
@@ -4,8 +4,19 @@ let button = document.querySelector('button');
 
 button.addEventListener('click', function () {
     //alert('Vous avez cliqué sur le bouton !');
-    let newElement = document.createElement("div2");
+    /*let newElement = document.createElement("div2");
     let p = document.getElementById("p2");
-    p.appendChild(newElement);
-   
+    p.appendChild(newElement);*/
+
+    let button = document.getElementById('monBouton')
+    let div1 = document.getElementById('div1')
+    console.log(div1)
+
+    button = addEventListener('click, function(){
+    let p2 = document.getElementById('p2')
+    let div2 = document.getElementById('div2')
+    div2.appenChild(p2);
+    console.log(div1)
+    console.log(div2)
 });
+    
