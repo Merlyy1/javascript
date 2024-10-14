4a3b37d68126e1135e5a15cc31c8e0a7d5bbdb5f
Merlyy1 <122520926+Merlyy1@users.noreply.github.com>
2024-10-14 16:51:42 +0200

    exo2.js

--- regex_merly/exo2.js
+++ regex_merly/exo2.js
@@ -3,9 +3,9 @@ phoneForm.addEventListener('submit', function(event) {
     event.preventDefault(); // Empêche la soumission réelle du formulaire
     let phone = document.getElementById('adresseip').value;
     phone = phone.replace(/<[^>]?>/g, ""); // on remplace les balises html par un espace
-    if (/^[1-9]([0-9]){1,2}[.- ]?([1-9]([0-9]){1,2}){3}$/.test(phone)) 
+    if (/^[1-9]([0-9]){1,2}[ .-]?([0-9][ .-]?|[1-9]([0-9]){1,2})[ .-]?([0-9][ .-]?|[1-9]([0-9]){1,2})[ .-]?([0-9][ .-]?|[1-9]([0-9]){1,2})$/.test(phone)) 
     {
-        document.getElementById('result').innerHTML = 'Ladresse ' + phone + ' est un mail <strong>valide</strong> !';
+        document.getElementById('result').innerHTML = 'Ladresse ' + phone + ' est une adresse <strong>valide</strong> !';
     } 
     else 
     {
@@ -13,3 +13,4 @@ phoneForm.addEventListener('submit', function(event) {
     }
 });
 
+///^[1-9]([0-9]){1,2}[.- ]?([1-9]([0-9]){1,2}){3}$/
