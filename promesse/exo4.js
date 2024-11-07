a8dd0bc9595791045aad6d33a85cbc3b2a4a354d
Merlyy1 <122520926+Merlyy1@users.noreply.github.com>
2024-11-07 12:32:08 +0100

    Delete promesse directory

--- promesse/exo4.js
+++
@@ -1,8 +0,0 @@
-const promesse = new Promise((resolve,reject)=>{
-    const aleatoire = Math.trunc((Math.random()*10)+1);
-    if (aleatoire<=5)resolve("entre 5 et 1")
-        else reject("supérieure à 5")
-
-})
-
-console.log(promesse);
