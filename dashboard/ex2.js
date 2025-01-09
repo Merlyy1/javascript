7ad1a394cba541903a619e91cee04063c867abab
Merlyy1 <122520926+Merlyy1@users.noreply.github.com>
2025-01-09 13:12:27 +0100

    Add files via upload

---
+++ dashboard/ex2.js
@@ -0,0 +1,12 @@
+class Afficheur{
+    constructor(id,nom,localite,message){
+        this.id = id;
+        this.nom = nom;
+        this.localite = localite;
+        this.message = message;
+    }
+
+    afficherInfos(){
+        
+    }
+}
